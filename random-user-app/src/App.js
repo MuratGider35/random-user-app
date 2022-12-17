17e8ffbec46e43113392bd74cc2c77019c3348a6
MuratGider35 <m.gider35@gmail.com>
2022-12-18 00:01:56 +0300

    build

--- random-user-app/src/App.js
+++ random-user-app/src/App.js
@@ -1,43 +1,61 @@
+import React from "react";
 import "./App.css";
-import axios from "axios";
 import { useState, useEffect } from "react";
-import mail from "./assets/email.svg";
+import axios from "axios";
+import emailicon from "./assets/email.svg";
+import locationicon from "./assets/location.svg";
+import phoneicon from "./assets/phone.svg";
 
-function App() {
-  const [ischange, setIschange] = useState(true);
-  const [user, setUser] = useState();
-  const randomUser = async () => {
+const App = () => {
+  const [user, setUser] = useState("");
+
+  const getUser = async () => {
     const url = "https://randomuser.me/api/";
-    const { data } = await axios(url);
-    console.log(data.results[0]);
-    setUser(data.results[0]);
+    const data = await axios(url);
+    setUser(data.data.results[0]);
   };
 
   useEffect(() => {
-    randomUser();
-  }, [ischange]);
-
+    getUser();
+  }, []);
+  const { name, location, email, phone, picture, dob, registered } = user;
   return (
-    <div className="App">
-      <div className="container">
-        <div className="image">
-          <div>
-            <img src={user.picture.medium} alt="" />
-          </div>
-          <h1>
-            {user?.name.first} {user?.name.last}
-          </h1>
-          <div className="mail">
-            <div>
-              <img src={mail} width="40px" alt="" />
-            </div>
-            <p>{user.email}</p>
-          </div>
+    <div className="container">
+      <div className="photo">
+        <div>
+          <img className="img" src={picture?.medium} alt="" />
         </div>
+        <p>
+          {name?.first} {name?.last}
+        </p>
       </div>
 
-      <button onClick={() => setIschange(!ischange)}>change</button>
+      <div className="mails">
+        <div>
+          <img src={emailicon} width="40px" alt="" />
+        </div>
+        <p>{email}</p>
+      </div>
+
+      <div className="phones">
+        <div>
+          <img src={phoneicon} width="40px" alt="" />
+        </div>
+        <p>{phone}</p>
+      </div>
+
+      <div className="locations">
+        <div>
+          <img src={locationicon} width="40px" alt="" />
+        </div>
+        <p>{location?.city}</p>
+      </div>
+
+      <p>Age:{dob?.age}</p>
+      <p>Register Date: {new Date(registered?.date).toLocaleDateString()}</p>
+      <button onClick={getUser}>Random User</button>
     </div>
   );
-}
+};
+
 export default App;
